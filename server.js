9b7cbd4a6158463406b8190b054df4c9178fa74b
Mónica Soberón Zubía <monicasoberon2747@gmail.com>
2025-04-28 12:31:40 -0600

    quiz

--- server.js
+++ server.js
@@ -4,13 +4,12 @@ const errorHandler = require('./src/middleware/error');
 
 const app = express();
 
-// MUY IMPORTANTE: Hacer que Express entienda JSON antes de las rutas
+// Hacer que Express entienda JSON antes de las rutas
 app.use(express.json());
 
-// Para servir archivos estáticos como index.html
+// Servir archivos estáticos como index.html
 app.use(express.static('public'));
 
-// Tus rutas
 app.use('/api/auth', authRoutes);
 
 // Manejo de errores
